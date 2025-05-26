31560dca993793ddc89a013d669d1b9845f0496b
Enes <enesizzet4646@gmail.com>
2025-05-26 03:50:53 +0300

    güncel dosya yapısı (BF- login-register)

--- backend/src/main/java/com/eneskaydi/yazilimprojeyonetimi/Entity/User.java
+++ backend/src/main/java/com/eneskaydi/yazilimprojeyonetimi/Entity/User.java
@@ -17,6 +17,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class User {
 
+    //
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // Kullanıcının benzersiz kimliği
